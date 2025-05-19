5083f402a1ff85309ce549edfc80e617c33ea474
Khemir Abderrahmen <35919483+abdoukhemir@users.noreply.github.com>
2025-05-19 15:23:54 +0100

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\Flutter src\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\Project\\PRojectSems2\\Test2\\image-classification-mango-disease-detection-flutter-main\\my_tflit_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\Flutter src\\src\\flutter"
+  "PROJECT_DIR=F:\\Project\\PRojectSems2\\Test2\\image-classification-mango-disease-detection-flutter-main\\my_tflit_app"
+  "FLUTTER_ROOT=E:\\Flutter src\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\Project\\PRojectSems2\\Test2\\image-classification-mango-disease-detection-flutter-main\\my_tflit_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\Project\\PRojectSems2\\Test2\\image-classification-mango-disease-detection-flutter-main\\my_tflit_app"
+  "FLUTTER_TARGET=F:\\Project\\PRojectSems2\\Test2\\image-classification-mango-disease-detection-flutter-main\\my_tflit_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9jNGNkNDhlMTg2NDYwYjMyZDQ0NTg1Y2UzYzEwMzI3MWFiNjc2MzU1Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\Project\\PRojectSems2\\Test2\\image-classification-mango-disease-detection-flutter-main\\my_tflit_app\\.dart_tool\\package_config.json"
+)
